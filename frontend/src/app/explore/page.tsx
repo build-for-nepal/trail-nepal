8ccfa0d12999ce3ed656f0417f9c51abf470de04
abiral-k <justabiral@gmail.com>
2026-04-16 14:15:09 +0545

    fix: remove unnecessary comments

--- frontend/src/app/explore/page.tsx
+++ frontend/src/app/explore/page.tsx
@@ -1,8 +1,6 @@
 import { ExploreHeader } from "@/components/explore/header/ExploreHeader";
 import ExploreLayout from "@/components/explore/treks/ExploreLayout";
 import Footer from "@/components/explore/Footer";
-// import Footer from "@/components/layout/footer/Footer";
-// import ExploreLayout from "@/components/explore/ExploreLayout";
 
 export default function ExplorePage() {
   return (
