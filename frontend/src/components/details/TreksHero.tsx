c23131433bbe9bf079e68d88b4f12e215d7dff7d
skh502 <sagar.khanal502@gmail.com>
2026-04-29 10:59:37 +0545

    fix: detail content responsive

--- frontend/src/components/details/TreksHero.tsx
+++ frontend/src/components/details/TreksHero.tsx
@@ -40,21 +40,57 @@ const TreksHero = ({ trekId }: Props) => {
     },
   ];
 
+  const chunkSize = 2;
+  const rows = [];
+  for (let i = 0; i < stats.length; i += chunkSize) {
+    rows.push(stats.slice(i, i + chunkSize));
+  }
+
   return (
     <section className="w-full flex flex-col">
-      <div className="page-wrapper bg-[#4276b2] w-full h-[100px] md:h-[140px] flex items-center justify-center">
-        <div className="page-wrapper w-full  mx-auto px-2 sm:px-8">
-          <div className="flex flex-row justify-between md:justify-around items-center w-full">
+      <div className="page-wrapper bg-[#4276b2] w-full py-6 md:py-8">
+        <div className="page-wrapper w-full mx-auto px-2 sm:px-8">
+          {/* mobile view */}
+          <div className="flex flex-col gap-6 md:hidden">
+            {rows.map((row, rowIndex) => (
+              <div
+                key={rowIndex}
+                className={`grid grid-cols-2 gap-4 ${
+                  row.length === 1 ? "flex justify-center" : ""
+                }`}
+              >
+                {row.map((stat) => (
+                  <div
+                    className={`flex flex-col items-center justify-center px-1 ${
+                      row.length === 1 ? "col-span-2 max-w-50 mx-auto" : ""
+                    }`}
+                    key={stat.title}
+                  >
+                    <div className="mb-1 md:mb-2">{stat.icon}</div>
+                    <div className="text-[16px] sm:text-[16px] font-bold text-white tracking-wide mb-0.5 md:mb-1 text-center whitespace-nowrap">
+                      {stat.title}
+                    </div>
+                    <div className="text-[12px] sm:text-[12px] text-white/90 text-center whitespace-nowrap">
+                      {stat.value}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+
+          {/* desktop view - from md */}
+          <div className="hidden md:flex flex-row justify-around items-center w-full">
             {stats.map((stat) => (
               <div
                 className="flex flex-col items-center justify-center px-1"
                 key={stat.title}
               >
                 <div className="mb-1 md:mb-2">{stat.icon}</div>
-                <div className="text-[12px] sm:text-[14px] md:text-[22px] font-bold text-white tracking-wide mb-0.5 md:mb-1 text-center whitespace-nowrap">
+                <div className="text-[12px] sm:text-[14px] md:text-[18px] lg:text-[22px] font-bold text-white tracking-wide mb-0.5 md:mb-1 text-center whitespace-nowrap">
                   {stat.title}
                 </div>
-                <div className="text-[9px] sm:text-[12px] md:text-sm text-white/90 text-center whitespace-nowrap">
+                <div className="text-[9px] sm:text-[12px] md:text-[13px] lg:text-[22px] text-white/90 text-center whitespace-nowrap">
                   {stat.value}
                 </div>
               </div>
