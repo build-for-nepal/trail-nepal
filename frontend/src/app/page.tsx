9899d8f0932aa27bb2774172add1ea9286edf979
skh502 <sagar.khanal502@gmail.com>
2026-05-15 10:59:42 +0545

    fix: footer spacing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <PopularTrekSection />
       <WhyTrialNepalSection />
       {/* <EstimateCostSection /> */}
-      <Footer />
+      <Footer isMainDisplay={true} />
     </main>
   );
 }
